329c9569d59f4d9dd731a48a2c01d9476b4e9532
gpborla+github@gmail.com <gpborla+github@gmail.com>
2018-04-13 00:50:57 +0800

    updated hibernate config file to update DB schema. refactored save contact process to save(contact). fetch and display contact with hibernate

--- src/main/java/com/teamgp/contactmgr/Application.java
+++ src/main/java/com/teamgp/contactmgr/Application.java
@@ -2,12 +2,15 @@ package com.teamgp.contactmgr;
 
 import com.teamgp.contactmgr.model.Contact;
 import com.teamgp.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class Application {
     //hold a reuseable reference to a SessionFactory since we only need one
@@ -24,8 +27,36 @@ public class Application {
                 .withEmail("rama@java.com")
                 .withPhone(773464347L)
                 .build();
-        System.out.println(contact);
 
+        save(contact);
+
+        //display a list of contacts
+        for(Contact c: fetchAllContacts()){
+            System.out.println(c);
+        }
+
+        //display a list of contact using Java Stream
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts(){
+        //open session
+        Session session = sessionFactory.openSession();
+
+        //create a criteria object
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        //get a list of Contact objects according to the Criteria object
+        List<Contact> contacts = criteria.list();
+
+        //close session
+        session.close();
+
+        return contacts;
+    }
+
+    private static void save(Contact contact){
         //open session
         Session session = sessionFactory.openSession();
 
