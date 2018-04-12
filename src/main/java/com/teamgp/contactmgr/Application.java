c7efbe745698342a022bd37dcbb49add5ae6d925
gpborla+github@gmail.com <gpborla+github@gmail.com>
2018-04-12 21:53:36 +0800

    edited ignore file. saving data with hibernate

--- src/main/java/com/teamgp/contactmgr/Application.java
+++ src/main/java/com/teamgp/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.teamgp.contactmgr;
 
 import com.teamgp.contactmgr.model.Contact;
 import com.teamgp.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,7 +11,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     //hold a reuseable reference to a SessionFactory since we only need one
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory(){
         //create a StandardServiceRegistry
@@ -24,5 +25,20 @@ public class Application {
                 .withPhone(773464347L)
                 .build();
         System.out.println(contact);
+
+        //open session
+        Session session = sessionFactory.openSession();
+
+        //begin a transaction
+        session.beginTransaction();
+
+        //use the session to save the contact
+        session.save(contact);
+
+        //commit the transaction
+        session.getTransaction().commit();
+
+        //close the session
+        session.close();
     }
 }
